3f3c3e66cb3feefaff08f224b8b4914f9547c75e
José Guerreiro <jose-guerreiro@live.com.pt>
2014-10-18 22:28:38 +0100

    Added scripts

---
+++ Ragdoll Exporter/Editor/RagdollExporter.cs
@@ -0,0 +1,218 @@
+﻿/*
+//  Copyright (c) 2014 José Guerreiro. All rights reserved.
+//
+//  MIT license, see http://www.opensource.org/licenses/mit-license.php
+//  
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//  
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//  
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+*/
+
+using System.IO;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+using UnityEditor;
+
+public class RagdollExporter : ScriptableWizard
+{
+    private string path = "Ragdoll";
+    public GameObject rootBone = null;
+
+    [MenuItem("Tools/Ragdoll Exporter")]
+    static void CreateWindow()
+    {
+        ScriptableWizard.DisplayWizard("Select root bone of ragdoll:", typeof(RagdollExporter), "Next");
+    }
+
+    void OnWizardCreate()
+    {
+        if ((Selection.gameObjects == null) || (Selection.gameObjects.Length != 1))
+        {
+            EditorUtility.DisplayDialog("Wrong selection", "Please select root bone to export the ragdoll", "OK");
+            return;
+        }
+        path = EditorUtility.SaveFilePanel("Save Ragdoll", "Assets", "Ragdoll", "xml");
+        GameObject gameObject = rootBone;
+        List<RagdollJoint> ragdollJoints = new List<RagdollJoint>();
+        
+        CharacterJoint[] jointsFromChildren = gameObject.GetComponents<CharacterJoint>();
+        if (jointsFromChildren.Length == 0)
+        {
+            Collider rootCollider = gameObject.GetComponent<Collider>();
+            Rigidbody rootRigidBody = gameObject.GetComponent<Rigidbody>();
+
+            if(rootCollider == null || rootRigidBody == null)
+            {
+                Debug.LogError("Ragdoll Exporter: root bone needs a collider and a rigidbody!");
+                return;
+            }
+
+            BoxColliderSettings boxColliderSettings = null;
+            SphereColliderSettings sphereColliderSettings = null;
+            CapsuleColliderSettings capsuleColliderSettings = null;
+            if (rootCollider is BoxCollider)
+            {
+                BoxCollider cast = (BoxCollider)rootCollider;
+                boxColliderSettings = new BoxColliderSettings();
+                boxColliderSettings.center = cast.center.ToString("G4");
+                boxColliderSettings.size = cast.size.ToString("G4");
+            }
+            else if (rootCollider is SphereCollider)
+            {
+                SphereCollider cast = (SphereCollider)rootCollider;
+                sphereColliderSettings = new SphereColliderSettings();
+                sphereColliderSettings.center = cast.center.ToString("G4");
+                sphereColliderSettings.radius = cast.radius;
+            }
+            else if (rootCollider is CapsuleCollider)
+            {
+                CapsuleCollider cast = (CapsuleCollider)rootCollider;
+                capsuleColliderSettings = new CapsuleColliderSettings();
+                capsuleColliderSettings.center = cast.center.ToString("G4");
+                capsuleColliderSettings.radius = cast.radius;
+                capsuleColliderSettings.height = cast.height;
+                capsuleColliderSettings.direction = cast.direction;
+            }
+
+            RigidbodySettings rigidbodySettings = new RigidbodySettings();
+            rigidbodySettings.mass = rootRigidBody.mass;
+
+            RagdollJoint ragdollJoint = new RagdollJoint();
+            ragdollJoint.boneName = gameObject.name;
+            ragdollJoint.characterJointSettings = null;
+            ragdollJoint.boxColliderSettings = boxColliderSettings;
+            ragdollJoint.sphereColliderSettings = sphereColliderSettings;
+            ragdollJoint.capsuleColliderSettings = capsuleColliderSettings;
+            ragdollJoint.rigidbodySettings = rigidbodySettings;
+            ragdollJoints.Add(ragdollJoint);
+        }
+        else
+        {
+            EditorUtility.DisplayDialog("Wrong selection", "Please select root bone to export the ragdoll", "OK");
+            return;
+        }
+
+        // Iterate over all character joints because it is easy to get rigidbody and collider from a joint but not the other way around
+        jointsFromChildren = gameObject.GetComponentsInChildren<CharacterJoint>();
+        Debug.Log("Ragdoll Exporter: " + gameObject.name + " joint processed");
+        foreach (CharacterJoint joint in jointsFromChildren)
+        {
+            Rigidbody rigidbody = joint.gameObject.GetComponent<Rigidbody>();
+            Collider collider = joint.gameObject.GetComponent<Collider>();
+            if (rigidbody == null || collider == null)
+            {
+                Debug.LogWarning("Ragdoll Exporter: bone with CharacterJoint is missing a collider or rigidbody (" + joint.name + ")");
+                continue;
+            }
+
+            CharacterJointSettings characterJointSettings = new CharacterJointSettings();
+            characterJointSettings.connectedBody = joint.connectedBody.name;
+
+            characterJointSettings.anchor = joint.anchor.ToString("G4");
+            characterJointSettings.axis = joint.axis.ToString("G4");
+            characterJointSettings.connectedAnchor = joint.connectedAnchor.ToString("G4");
+            characterJointSettings.swingAxis = joint.swingAxis.ToString("G4");
+
+            characterJointSettings.lowTwistLimit_Bounciness = joint.lowTwistLimit.bounciness;
+            characterJointSettings.lowTwistLimit_Damper = joint.lowTwistLimit.damper;
+            characterJointSettings.lowTwistLimit_Limit = joint.lowTwistLimit.limit;
+            characterJointSettings.lowTwistLimit_Spring = joint.lowTwistLimit.spring;
+
+            characterJointSettings.highTwistLimit_Bounciness = joint.highTwistLimit.bounciness;
+            characterJointSettings.highTwistLimit_Damper = joint.highTwistLimit.damper;
+            characterJointSettings.highTwistLimit_Limit = joint.highTwistLimit.limit;
+            characterJointSettings.highTwistLimit_Spring = joint.highTwistLimit.spring;
+
+            characterJointSettings.swing1Limit_Bounciness = joint.swing1Limit.bounciness;
+            characterJointSettings.swing1Limit_Damper = joint.swing1Limit.damper;
+            characterJointSettings.swing1Limit_Limit = joint.swing1Limit.limit;
+            characterJointSettings.swing1Limit_Spring = joint.swing1Limit.spring;
+
+            characterJointSettings.swing2Limit_Bounciness = joint.swing2Limit.bounciness;
+            characterJointSettings.swing2Limit_Damper = joint.swing2Limit.damper;
+            characterJointSettings.swing2Limit_Limit = joint.swing2Limit.limit;
+            characterJointSettings.swing2Limit_Spring = joint.swing2Limit.spring;
+
+            BoxColliderSettings boxColliderSettings = null;
+            SphereColliderSettings sphereColliderSettings = null;
+            CapsuleColliderSettings capsuleColliderSettings = null;
+            if (collider is BoxCollider)
+            {
+                BoxCollider cast = (BoxCollider)collider;
+                boxColliderSettings = new BoxColliderSettings();
+                boxColliderSettings.center = cast.center.ToString("G4");
+                boxColliderSettings.size = cast.size.ToString("G4");
+            }
+            else if (collider is SphereCollider)
+            {
+                SphereCollider cast = (SphereCollider)collider;
+                sphereColliderSettings = new SphereColliderSettings();
+                sphereColliderSettings.center = cast.center.ToString("G4");
+                sphereColliderSettings.radius = cast.radius;
+            }
+            else if (collider is CapsuleCollider)
+            {
+                CapsuleCollider cast = (CapsuleCollider)collider;
+                capsuleColliderSettings = new CapsuleColliderSettings();
+                capsuleColliderSettings.center = cast.center.ToString("G4");
+                capsuleColliderSettings.radius = cast.radius;
+                capsuleColliderSettings.height = cast.height;
+                capsuleColliderSettings.direction = cast.direction;
+            }
+
+            RigidbodySettings rigidbodySettings = new RigidbodySettings();
+            rigidbodySettings.mass = rigidbody.mass;
+
+            RagdollJoint ragdollJoint = new RagdollJoint();
+            ragdollJoint.boneName = joint.name;
+            ragdollJoint.characterJointSettings = characterJointSettings;
+            ragdollJoint.boxColliderSettings = boxColliderSettings;
+            ragdollJoint.sphereColliderSettings = sphereColliderSettings;
+            ragdollJoint.capsuleColliderSettings = capsuleColliderSettings;
+            ragdollJoint.rigidbodySettings = rigidbodySettings;
+            ragdollJoints.Add(ragdollJoint);
+
+            Debug.Log("Ragdoll Exporter: " + joint.name + " joint processed");
+        }
+
+        if(path == null || path == "")
+        {
+            Debug.Log("Ragdoll Exporter: operation cancelled");
+            return;
+        }
+        Ragdoll rD = new Ragdoll();
+        rD.ragdollJoints = ragdollJoints.ToArray();
+        string xml = XMLSerializer.SerializeObject(rD);
+        using (StreamWriter writer = new StreamWriter(path, false))
+        {
+            try
+            {
+                writer.Write(xml);
+            }
+            catch (System.Exception ex)
+            {
+                string msg = " threw:\n" + ex.ToString();
+                Debug.LogError(msg);
+                EditorUtility.DisplayDialog("Error on export", msg, "OK");
+            }
+        }
+
+        Debug.Log("Ragdoll Exporter: " + (jointsFromChildren.Length + 1).ToString() + " joints processed");
+        Debug.Log("Success!");
+    }
+}
---
+++ Ragdoll Exporter/RagdollLoader.cs
@@ -0,0 +1,332 @@
+﻿/*
+//  Copyright (c) 2014 José Guerreiro. All rights reserved.
+//
+//  MIT license, see http://www.opensource.org/licenses/mit-license.php
+//  
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//  
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//  
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+*/
+
+using UnityEngine;
+using System.Collections;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+
+[XmlRoot]
+public class Ragdoll
+{
+    [XmlElement]
+    public RagdollJoint[] ragdollJoints;
+}
+public class RagdollJoint
+{
+    [XmlAttribute]
+    public string boneName;
+    [XmlElement]
+    public CharacterJointSettings characterJointSettings;
+    [XmlElement]
+    public BoxColliderSettings boxColliderSettings;
+    [XmlElement]
+    public SphereColliderSettings sphereColliderSettings;
+    [XmlElement]
+    public CapsuleColliderSettings capsuleColliderSettings;
+    [XmlElement]
+    public RigidbodySettings rigidbodySettings;
+}
+public class CharacterJointSettings
+{
+    [XmlAttribute]
+    public string connectedBody;
+
+    [XmlAttribute]
+    public string anchor;
+    [XmlAttribute]
+    public string axis;
+
+    [XmlAttribute]
+    public string connectedAnchor;
+    [XmlAttribute]
+    public string swingAxis;
+
+    [XmlAttribute]
+    public float lowTwistLimit_Limit;
+    [XmlAttribute]
+    public float lowTwistLimit_Bounciness;
+    [XmlAttribute]
+    public float lowTwistLimit_Spring;
+    [XmlAttribute]
+    public float lowTwistLimit_Damper;
+
+    [XmlAttribute]
+    public float highTwistLimit_Limit;
+    [XmlAttribute]
+    public float highTwistLimit_Bounciness;
+    [XmlAttribute]
+    public float highTwistLimit_Spring;
+    [XmlAttribute]
+    public float highTwistLimit_Damper;
+
+    [XmlAttribute]
+    public float swing1Limit_Limit;
+    [XmlAttribute]
+    public float swing1Limit_Bounciness;
+    [XmlAttribute]
+    public float swing1Limit_Spring;
+    [XmlAttribute]
+    public float swing1Limit_Damper;
+
+    [XmlAttribute]
+    public float swing2Limit_Limit;
+    [XmlAttribute]
+    public float swing2Limit_Bounciness;
+    [XmlAttribute]
+    public float swing2Limit_Spring;
+    [XmlAttribute]
+    public float swing2Limit_Damper;
+}
+public class BoxColliderSettings
+{
+    [XmlAttribute]
+    public string center;
+    [XmlAttribute]
+    public string size;
+}
+public class SphereColliderSettings
+{
+    [XmlAttribute]
+    public string center;
+    [XmlAttribute]
+    public float radius;
+}
+public class CapsuleColliderSettings
+{
+    [XmlAttribute]
+    public string center;
+    [XmlAttribute]
+    public float height;
+    [XmlAttribute]
+    public float radius;
+    [XmlAttribute]
+    public int direction;
+}
+public class RigidbodySettings
+{
+    [XmlAttribute]
+    public float mass;
+}
+
+public static class MathHelper
+{
+    public static Vector3 FromString(string value)
+    {
+        string[] temp = value.Replace(" ", "").Replace("(", "").Replace(")", "").Split(',');
+        Vector3 vector = new Vector3();
+        vector.x = float.Parse(temp[0]);
+        vector.y = float.Parse(temp[1]);
+        vector.z = float.Parse(temp[2]);
+        return vector;
+    }
+}
+
+public class RagdollLoader : MonoBehaviour {
+    public Object ragdoll;
+    public GameObject target;
+
+	void Start () {
+        Load(ragdoll.ToString(), target, false);
+	}
+	
+    public static void Load(string ragdoll, GameObject target, bool editor)
+    {
+        Dictionary<string, RagdollJoint> ragdollJointsDictionary = new Dictionary<string, RagdollJoint>();
+        RagdollJoint[] ragdollJoints = XMLSerializer.DeserializeObject<Ragdoll>(ragdoll).ragdollJoints;
+        foreach (RagdollJoint rJ in ragdollJoints)
+        {
+            ragdollJointsDictionary[rJ.boneName] = rJ;
+        }
+
+        if (ragdollJointsDictionary.ContainsKey(target.name))
+        {
+            // Root bone
+
+            if (target.GetComponent<CharacterJoint>() != null)
+                DestroyImmediate(target.GetComponent<CharacterJoint>());
+            if (target.GetComponent<Collider>() != null)
+                DestroyImmediate(target.GetComponent<Collider>());
+            if(target.GetComponent<Rigidbody>() != null)
+                DestroyImmediate(target.GetComponent<Rigidbody>());
+            Dictionary<string, GameObject> children = new Dictionary<string, GameObject>();
+            foreach (Transform t in target.GetComponentsInChildren<Transform>())
+            {
+                children[t.gameObject.name] = t.gameObject;
+
+                if (t.gameObject.GetComponent<CharacterJoint>() != null)
+                    DestroyImmediate(t.gameObject.GetComponent<CharacterJoint>());
+                if (t.gameObject.GetComponent<Collider>() != null)
+                    DestroyImmediate(t.gameObject.GetComponent<Collider>());
+                if (t.gameObject.GetComponent<Rigidbody>() != null)
+                    DestroyImmediate(t.gameObject.GetComponent<Rigidbody>());
+            }
+            children[target.name] = target;
+
+            RagdollJoint rJ = ragdollJointsDictionary[target.name];
+
+            if (rJ.boxColliderSettings != null)
+            {
+                BoxCollider bC = target.AddComponent<BoxCollider>();
+                bC.center = MathHelper.FromString(rJ.boxColliderSettings.center);
+                bC.size = MathHelper.FromString(rJ.boxColliderSettings.size);
+            }
+            else if (rJ.sphereColliderSettings != null)
+            {
+                SphereCollider sC = target.AddComponent<SphereCollider>();
+                sC.center = MathHelper.FromString(rJ.sphereColliderSettings.center);
+                sC.radius = rJ.sphereColliderSettings.radius;
+            }
+            else if (rJ.capsuleColliderSettings != null)
+            {
+                CapsuleCollider cC = target.AddComponent<CapsuleCollider>();
+                cC.center = MathHelper.FromString(rJ.capsuleColliderSettings.center);
+                cC.radius = rJ.capsuleColliderSettings.radius;
+                cC.height = rJ.capsuleColliderSettings.height;
+                cC.direction = rJ.capsuleColliderSettings.direction;
+            }
+            else
+                Debug.LogError("Ragdoll Loader: collider type is not supported (" + target.name + ")");
+
+            Rigidbody rB = target.AddComponent<Rigidbody>();
+            rB.mass = rJ.rigidbodySettings.mass;
+
+            // Children
+
+            if (editor)
+            {
+                Debug.Log("Ragdoll Loader: " + target.name + " joint processed");
+            }
+
+            int count = 1;
+
+            foreach (GameObject gO in children.Values)
+            {
+                if (gO.name == target.name)
+                    continue;
+
+                if (ragdollJointsDictionary.ContainsKey(gO.name))
+                {
+                    rJ = ragdollJointsDictionary[gO.name];
+
+                    if (rJ.boxColliderSettings != null)
+                    {
+                        BoxCollider bC = gO.AddComponent<BoxCollider>();
+                        bC.center = MathHelper.FromString(rJ.boxColliderSettings.center);
+                        bC.size = MathHelper.FromString(rJ.boxColliderSettings.size);
+                    }
+                    else if (rJ.sphereColliderSettings != null)
+                    {
+                        SphereCollider sC = gO.AddComponent<SphereCollider>();
+                        sC.center = MathHelper.FromString(rJ.sphereColliderSettings.center);
+                        sC.radius = rJ.sphereColliderSettings.radius;
+                    }
+                    else if (rJ.capsuleColliderSettings != null)
+                    {
+                        CapsuleCollider cC = gO.AddComponent<CapsuleCollider>();
+                        cC.center = MathHelper.FromString(rJ.capsuleColliderSettings.center);
+                        cC.radius = rJ.capsuleColliderSettings.radius;
+                        cC.height = rJ.capsuleColliderSettings.height;
+                        cC.direction = rJ.capsuleColliderSettings.direction;
+                    }
+                    else
+                        Debug.LogError("Ragdoll Loader: collider type is not supported (" + gO.name + ")");
+
+                    rB = gO.AddComponent<Rigidbody>();
+                    rB.mass = rJ.rigidbodySettings.mass;
+
+                    if (editor)
+                    {
+                        Debug.Log("Ragdoll Loader: " + gO.name + " joint processed");
+                    }
+
+                    count++;
+                }
+                else if (editor)
+                {
+                    //Debug.LogWarning("AssignRagdoll: bone name not found in ragdoll (" + gO.name + ")");
+                }
+            }
+
+            foreach (GameObject gO in children.Values)
+            {
+                if (gO.name == target.name)
+                    continue;
+
+                if (ragdollJointsDictionary.ContainsKey(gO.name))
+                {
+                    rJ = ragdollJointsDictionary[gO.name];
+
+                    CharacterJoint cJ = gO.AddComponent<CharacterJoint>();
+
+                    if (children.ContainsKey(rJ.characterJointSettings.connectedBody))
+                        cJ.connectedBody = children[rJ.characterJointSettings.connectedBody].rigidbody;
+                    else
+                        Debug.LogError("Ragdoll Loader: connected joint not found (" + rJ.characterJointSettings.connectedBody + " with " + gO.name + ")");
+
+                    cJ.anchor = MathHelper.FromString(rJ.characterJointSettings.anchor);
+                    cJ.axis = MathHelper.FromString(rJ.characterJointSettings.axis);
+                    cJ.swingAxis = MathHelper.FromString(rJ.characterJointSettings.swingAxis);
+                    cJ.connectedAnchor = MathHelper.FromString(rJ.characterJointSettings.connectedAnchor);
+
+                    SoftJointLimit sJL = new SoftJointLimit();
+                    sJL.bounciness = rJ.characterJointSettings.lowTwistLimit_Bounciness;
+                    sJL.limit = rJ.characterJointSettings.lowTwistLimit_Limit;
+                    sJL.damper = rJ.characterJointSettings.lowTwistLimit_Damper;
+                    sJL.spring = rJ.characterJointSettings.lowTwistLimit_Spring;
+                    cJ.lowTwistLimit = sJL;
+
+                    sJL = new SoftJointLimit();
+                    sJL.bounciness = rJ.characterJointSettings.highTwistLimit_Bounciness;
+                    sJL.limit = rJ.characterJointSettings.highTwistLimit_Limit;
+                    sJL.damper = rJ.characterJointSettings.highTwistLimit_Damper;
+                    sJL.spring = rJ.characterJointSettings.highTwistLimit_Spring;
+                    cJ.highTwistLimit = sJL;
+
+                    sJL = new SoftJointLimit();
+                    sJL.bounciness = rJ.characterJointSettings.swing1Limit_Bounciness;
+                    sJL.limit = rJ.characterJointSettings.swing1Limit_Limit;
+                    sJL.damper = rJ.characterJointSettings.swing1Limit_Damper;
+                    sJL.spring = rJ.characterJointSettings.swing1Limit_Spring;
+                    cJ.swing1Limit = sJL;
+
+                    sJL = new SoftJointLimit();
+                    sJL.bounciness = rJ.characterJointSettings.swing2Limit_Bounciness;
+                    sJL.limit = rJ.characterJointSettings.swing2Limit_Limit;
+                    sJL.damper = rJ.characterJointSettings.swing2Limit_Damper;
+                    sJL.spring = rJ.characterJointSettings.swing2Limit_Spring;
+                    cJ.swing2Limit = sJL;
+                }
+            }
+
+            if (editor)
+            {
+                Debug.Log("Ragdoll Loader: " + count.ToString() + " joints processed");
+                Debug.Log("Success!");
+            }
+        }
+        else
+            Debug.LogError("Ragdoll Loader: target needs to be the root bone!");
+    }
+}
